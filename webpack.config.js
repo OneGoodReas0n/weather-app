3e778d7a975644d0634b91780776c82313b62fd3
Illia Korobov <korobovi4@gmail.com>
2020-03-17 09:37:35 +0100

    update: changes in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,81 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TSLintPlugin = require("tslint-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const sass = require('sass');
 
 module.exports = {
-  entry: "./src/js/index.ts",
+   entry: './src/js/index.js',
 
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "build.js"
-  },
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'build.js',
+   },
 
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "ts-loader"
-        }
-      },
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: [
+               {
+                  loader: 'babel-loader',
+                  options: {
+                     presets: ['@babel/preset-env'],
+                  },
+               },
+               {
+                  loader: 'eslint-loader',
+               },
+            ],
+         },
 
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
+         {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+               { loader: MiniCssExtractPlugin.loader },
 
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              implementation: require("sass")
-            }
-          }
-        ]
-      },
+               {
+                  loader: 'css-loader',
+               },
+               {
+                  loader: 'sass-loader',
+                  options: {
+                     implementation: sass,
+                  },
+               },
+            ],
+         },
 
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "images"
-            }
-          }
-        ]
-      },
+         {
+            test: /\.(png|jpe?g|gif|svg)$/,
+            use: [
+               {
+                  loader: 'file-loader',
+                  options: {
+                     outputPath: 'images',
+                  },
+               },
+            ],
+         },
 
-      {
-        test: /\.(woff|woff2|ttf|otf|eot)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "fonts"
-            }
-          }
-        ]
-      }
-    ]
-  },
+         {
+            test: /\.(woff|woff2|ttf|otf|eot)$/,
+            use: [
+               {
+                  loader: 'file-loader',
+                  options: {
+                     outputPath: 'fonts',
+                  },
+               },
+            ],
+         },
+      ],
+   },
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "style.css"
-    }),
+   plugins: [
+      new MiniCssExtractPlugin({
+         filename: 'style.css',
+      }),
+   ],
 
-    new TSLintPlugin({
-      files: ["./src/**/*.ts"]
-    })
-  ],
-
-  mode: "development"
+   mode: 'development',
 };
