feaa020339eefde7f8047056642f143759b70e12
Illia Korobov <korobovi4@gmail.com>
2020-03-30 20:34:11 +0200

    update: changes in weatherAPI

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -1,12 +1,17 @@
 import tockens from '../keys/tockens';
+import { safeWeatherToCache, weatherSerialize } from '../utils/functions';
 
-const getForcast = async (city, state) => {
-   const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city},${state}&appid=${tockens.weatherTOCKEN}`
-   );
-   const data = await response.json();
+const getForcast = async (city, state, units) => {
+   const path = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${state}&units=${
+      units === 'C' ? 'metric' : 'imperial'
+   }&appid=${tockens.weatherTOCKEN}`;
 
-   return data;
+   const response = await fetch(path);
+   const weather = await response.json();
+   const serializedArr = weatherSerialize(weather);
+   safeWeatherToCache(serializedArr, units);
+   console.log('Weather API called !');
+   return weather;
 };
 
 export default getForcast;
