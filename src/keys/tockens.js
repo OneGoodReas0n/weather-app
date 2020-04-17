31a082228a220ca5a59f9258b4bceb47eca8fa3c
korobovi4@gmail.com <korobovi4@gmail.com>
2020-04-17 09:10:42 +0200

    update: remove keys from repository

--- src/keys/tockens.js
+++
@@ -1,9 +0,0 @@
-const tockens = {
-   WEATHER_TOCKEN: '4af043689a4e500a8fadc75b7f5147a4',
-   IPINFO_TOKEN: '7b61ace3d10e76',
-   MAPBOX_TOKEN:
-      'pk.eyJ1IjoicjNhc29uIiwiYSI6ImNrN3hlOTQ4ejBhY3Mzb3BwcmYwa3N1OXYifQ.OQ-T7-w6-m0BT7dIREOelw',
-   GEOCODING_TOKEN: 'AIzaSyCQxKy4hBAKRt3QC4qU_Sm_LiNNBD0dSWw'
-};
-
-export default tockens;
