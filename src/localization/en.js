b6b3e9f72ad7a8fca776f373338c00e94dd97ff8
korobovi4@gmail.com <korobovi4@gmail.com>
2020-04-20 22:35:35 +0200

    init: implement libraries with words for translation

---
+++ src/localization/en.js
@@ -0,0 +1,46 @@
+const en = {
+   hearder: {
+      searchInput: {
+         placeholder: 'Search city or ZIP',
+         home: 'Home',
+         voiceSearch: 'Voice search'
+      }
+   },
+   forecast: {
+      feelsLike: 'Feels like',
+      wind: 'Wind',
+      humidity: 'Humidity'
+   },
+
+   location: {
+      latitude: 'Latitude',
+      longitude: 'Longitude'
+   },
+
+   dayOfWeek: {
+      monday: 'Monday',
+      tuesday: 'Tuesday',
+      wednesday: 'Wednesday',
+      thursday: 'Thursday',
+      friday: 'Friday',
+      saturday: 'Saturday',
+      sunday: 'Sunday'
+   },
+
+   month: {
+      january: 'Jan',
+      february: 'Feb',
+      march: 'Mar',
+      april: 'Apr',
+      may: 'May',
+      june: 'Jun',
+      july: 'Jul',
+      august: 'Aug',
+      september: 'Sep',
+      october: 'Oct',
+      november: 'Nov',
+      december: 'Dec'
+   }
+};
+
+export default en;
