81efd981872e28ca53bb9a9551c9756b37b20eed
korobovi4@gmail.com <korobovi4@gmail.com>
2020-04-26 15:35:38 +0200

    fix: path to assets is fixed

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
                   loader: 'file-loader',
                   options: {
                      outputPath: 'assets',
-                     publicPath: '/assets'
+                     publicPath: './assets'
                   }
                }
             ]
